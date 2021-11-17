ace679b64db093a9d27b462d1da3a97c2fc044a4
Gillarion <oiskritsky@gmail.com>
2021-11-17 21:46:31 +0300

    feature: create DatePicker

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import InputSearch from './InputSearch';
+import InputSearchHotel from './Inputs/InputSearchHotel';
+import InputDate from './Inputs/InputDate';
 
 export default function Form({ hotelSearchData, setHotelSearchData }) {
   const [destination, setDestination] = useState(hotelSearchData);
+  const [dateRange, setDateRange] = useState({ startDate: null, endDate: null });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setHotelSearchData(destination);
   };
+
   return (
     <form className='form__desktop' onSubmit={handleSubmit}>
-      <div className='input__lable'>
-        <label htmlFor='search'>Your destination or hotel name</label>
-      </div>
       <section className='form-desktop__wrapper'>
         <div className='sbar__first'>
-          <InputSearch value={destination} onChange={(e) => setDestination(e.target.value)} />
+          <InputSearchHotel value={destination} onChange={(e) => setDestination(e.target.value)} />
         </div>
-        <button id='sendForm' type='submit'>
-          Search
-        </button>
+        <div className='sbar__second'>
+          <InputDate dateRange={dateRange} setDateRange={setDateRange} />
+        </div>
+        <input type='submit' id='sendForm' />
       </section>
     </form>
   );
